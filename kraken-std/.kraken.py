2956ae5724a063f7903b6619bf3276879ff424e0
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-05-13 23:17:23 +0000

    point to local versions of kraken

--- kraken-std/.kraken.py
+++ kraken-std/.kraken.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from kraken.common import buildscript
 
-buildscript(requirements=["kraken-std@."])
+buildscript(requirements=["kraken-common@../kraken-common", "kraken-core@../kraken-core", "kraken-std@."])
 
 import os
 
