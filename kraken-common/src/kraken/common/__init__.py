07c08d02b1deb7303aea5401ba0a558b92ddf13a
Niklas Rosenstein <niklas.rosenstein@helsing.ai>
2023-05-15 16:47:11 +0000

    release 0.20.1

--- kraken-common/src/kraken/common/__init__.py
+++ kraken-common/src/kraken/common/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.20.0"
+__version__ = "0.20.1"
 
 from . import path
 from ._argparse import propagate_argparse_formatter_to_subparser
--- kraken-core/src/kraken/core/__init__.py
+++ kraken-core/src/kraken/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.20.0"
+__version__ = "0.20.1"
 
 from kraken.core.address import Address
 from kraken.core.system.context import Context, ContextEvent
