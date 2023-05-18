5a93cdd67934597582669b5da8c98b257298e6b7
Callum Pearce <callum.pearce@helsing.ai>
2023-05-18 16:27:48 +0100

    update dependencies before attempting to package with helm

--- kraken-std/src/kraken/std/helm/helmapi.py
+++ kraken-std/src/kraken/std/helm/helmapi.py
@@ -23,7 +23,7 @@ def helm_package(
         raise ValueError("output_file and output_directory cannot both be set")
 
     with contextlib.ExitStack() as exit_stack:
-        command = ["helm", "package", str(chart_path)]
+        command = ["helm", "package", str(chart_path), "--dependency-update"]
 
         # We build into a temporary directory first.
         tempdir = Path(exit_stack.enter_context(tempfile.TemporaryDirectory()))
@@ -53,10 +53,20 @@ def helm_package(
     assert False
 
 
-def helm_registry_login(registry: str, username: str, password: str, insecure: bool = False) -> tuple[list[str], int]:
+def helm_registry_login(
+    registry: str, username: str, password: str, insecure: bool = False
+) -> tuple[list[str], int]:
     """Log into a Helm registry."""
 
-    command = ["helm", "registry", "login", registry, "-u", username, "--password-stdin"]
+    command = [
+        "helm",
+        "registry",
+        "login",
+        registry,
+        "-u",
+        username,
+        "--password-stdin",
+    ]
     if insecure:
         command += ["--insecure"]
     return command, sp.run(command, input=f"{password}\n".encode()).returncode
