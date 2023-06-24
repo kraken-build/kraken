d97f2628b18678bbf96613e4e29bf27980aaf26c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-06-24 23:22:20 +0200

    Fix Poetry integration test, but mark PDM test as failing because of bad hash

--- kraken-std/src/tests/integration/python/test_python.py
+++ kraken-std/src/tests/integration/python/test_python.py
@@ -42,7 +42,7 @@ def pypiserver(docker_service_manager: DockerServiceManager, tempdir: Path) -> s
     index_url = f"http://localhost:{PYPISERVER_PORT}/simple"
     docker_service_manager.run(
         "pypiserver/pypiserver:latest",
-        ["--passwords", "/.htpasswd", "-a", "update"],
+        ["--passwords", "/.htpasswd", "-a", "update", "--hash-algo", "sha256"],
         ports=[f"{PYPISERVER_PORT}:8080"],
         volumes=[f"{htpasswd.absolute()}:/.htpasswd"],
         detach=True,
@@ -55,21 +55,15 @@ def pypiserver(docker_service_manager: DockerServiceManager, tempdir: Path) -> s
 @pytest.mark.parametrize(
     "project_dir",
     [
-        pytest.param(
-            "poetry-project",
-            marks=pytest.mark.xfail(
-                reason="""
-                    There appears to be an issue with Poetry 1.2.x and Pypiserver where the hashsums don't add up.
-                    Example error message:
-
-                        Retrieved digest for link poetry_project-0.1.0-py3-none-any.whl(md5:6340bed3198ccf181970f82cf6220f78)
-                        not in poetry.lock metadata ['sha256:a2916a4e6ccb4c2f43f0ee9fb7fb1331962b9ec061f967c642fcfb9dbda435f3',
-                        'sha256:80a47720d855408d426e835fc6088ed3aba2d0238611e16b483efe8e063d71ee']
-                """  # noqa: E501
-            ),
-        ),
+        "poetry-project",
         "slap-project",
-        "pdm-project",
+        pytest.params(
+            "pdm-project",
+            marks=pytest.mark.xfail(
+                reason="PDM seems to have an issue with getting the right hash of the package from pypiserver "
+                "when locking."
+            )
+        )
     ],
 )
 @unittest.mock.patch.dict(os.environ, {})
@@ -99,6 +93,10 @@ def test__python_project_install_lint_and_publish(
     logger.info("Loading and executing Kraken project (%s)", tempdir / consumer_dir)
     Context.__init__(kraken_ctx, kraken_ctx.build_directory)
     kraken_ctx.load_project(directory=tempdir / consumer_dir)
+
+    # NOTE: The Slap project doesn't need an apply because we don't write the package index into the pyproject.toml.
+    kraken_ctx.execute([":apply"])
+
     kraken_ctx.execute([":python.install"])
     # TODO (@NiklasRosenstein): Test importing the consumer project.
 
