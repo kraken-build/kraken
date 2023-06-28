59f0f57dcee80198515f73c46d34bf2da04b332d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-06-28 21:56:52 +0200

    release 0.27.4

--- kraken-core/src/kraken/core/__init__.py
+++ kraken-core/src/kraken/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.27.3"
+__version__ = "0.27.4"
 
 from kraken.core.address import Address
 from kraken.core.system.context import Context, ContextEvent
--- kraken-wrapper/src/kraken/wrapper/__init__.py
+++ kraken-wrapper/src/kraken/wrapper/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "DEFAULT_CONFIG_PATH",
     "AuthModel",
 ]
-__version__ = "0.27.3"
+__version__ = "0.27.4"
