af8cb393e30d4068835210432a3892c2bd3c2777
James Baker <james.baker@helsing.ai>
2023-07-12 17:37:38 +0100

    draft: add libstdc++ to the PATH to ensure that mitmproxy is able to boot up

--- flake.nix
+++ flake.nix
@@ -17,13 +17,21 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        krakenwRaw = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./kraken-wrapper;
+          preferWheels = true;
+        };
+        krakenw = pkgs.writeShellScriptBin "krakenw" ''
+          # This is a hack! For Cargo proxying, Kraken depends on mitmproxy. Indirectly, mitmproxy as compiled for Linux relies
+          # on being able to use libstdc++, but because it is executed as a subprocess, it's not able to be found in a Nix Linux
+          # environment. For now, we can add libstdc++ to the LD_LIBRARY_PATH to ensure that it's linked correctly.
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}"
+          exec ${krakenwRaw}/bin/krakenw $@
+        '';
       in
       rec {
         packages = flake-utils.lib.flattenTree rec {
-          krakenw = (pkgs.poetry2nix.mkPoetryApplication {
-            projectDir = ./kraken-wrapper;
-            preferWheels = true;
-          });
+          krakenw = krakenw;
           default = krakenw;
         };
       }
