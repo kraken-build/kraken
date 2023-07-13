8e843dea8454132a57e97f0f26fcdd716d564863
Mihail Morosan <morosanmihail@outlook.com>
2023-07-13 14:44:08 +0100

    Add comment explaining features comma separation.

--- kraken-std/src/kraken/std/cargo/__init__.py
+++ kraken-std/src/kraken/std/cargo/__init__.py
@@ -325,6 +325,8 @@ def cargo_build(
         additional_args.append("--release")
     if features:
         additional_args.append("--features")
+        # `cargo build`` expects features to be comma separated, in one string.
+        # for example `cargo build --features abc,efg` instead of `cargo build --features abc efg`.
         additional_args.append(",".join(features))
 
     task = project.task(f"cargoBuild{mode.capitalize()}" if name is None else name, CargoBuildTask, group=group)
