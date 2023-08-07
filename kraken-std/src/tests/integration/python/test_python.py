ec52d6fcd18c78fa5c2672b561af547da969932f
Thomas Tanon <thomas.pellissier-tanon@helsing.ai>
2023-08-07 09:42:54 +0200

    Makes PDM integration test mandatory (#86)
    * Makes PDM integration test mandatory
    
    * Updated PR references in 0 changelogs.
    
    skip-checks: true
    
    ---------
    
    Co-authored-by: GitHub Action <github-action@users.noreply.github.com>

--- kraken-std/src/tests/integration/python/test_python.py
+++ kraken-std/src/tests/integration/python/test_python.py
@@ -55,19 +55,7 @@ def pypiserver(docker_service_manager: DockerServiceManager, tempdir: Path) -> s
 
 @pytest.mark.parametrize(
     "project_dir",
-    [
-        "poetry-project",
-        "slap-project",
-        pytest.param(
-            "pdm-project",
-            marks=pytest.mark.xfail(
-                reason="PDM seems to have an issue with getting the right hash of the package from pypiserver "
-                "when locking."
-            ),
-        ),
-        "rust-poetry-project",
-        "rust-pdm-project",
-    ],
+    ["poetry-project", "slap-project", "pdm-project", "rust-poetry-project", "rust-pdm-project"],
 )
 @unittest.mock.patch.dict(os.environ, {})
 def test__python_project_install_lint_and_publish(
