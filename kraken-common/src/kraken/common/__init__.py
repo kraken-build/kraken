e068b0aa780f381d9cd1364bb5b6191629686d56
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-09-14 16:40:54 +0200

    release 0.31.3

--- kraken-common/src/kraken/common/__init__.py
+++ kraken-common/src/kraken/common/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.31.2"
+__version__ = "0.31.3"
 
 from . import path
 from ._argparse import propagate_argparse_formatter_to_subparser
--- kraken-wrapper/src/kraken/wrapper/__init__.py
+++ kraken-wrapper/src/kraken/wrapper/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "DEFAULT_CONFIG_PATH",
     "AuthModel",
 ]
-__version__ = "0.31.2"
+__version__ = "0.31.3"
