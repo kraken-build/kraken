e403c6c4c8ac80114fa43626dbbf53e9ec074e33
Niklas Rosenstein <niklas.rosenstein@helsing.ai>
2023-12-14 00:56:18 +0100

    disable "Insert PR URL into new changelog entries" job in CI for now, fix docs .kraken.py

--- docs/.kraken.py
+++ docs/.kraken.py
@@ -6,9 +6,10 @@ project = Project.current()
 docs_venv = project.task("venv", DocsVenvTask)
 docs_venv.directory = project.build_directory / "docs-venev"
 
-docs_build = project.task("build", DocsTask)
+docs_build = project.task("_build", DocsTask)
 docs_build.venv = docs_venv.directory
 docs_build.mode = "build"
+project.group("build").add(docs_build)
 
 docs_serve = project.task("serve", DocsTask)
 docs_serve.venv = docs_venv.directory
