e81c48a154082ca0255cf834b5cc7cff7c47c4fa
Niklas Rosenstein <niklas.rosenstein@helsing.ai>
2023-12-14 00:36:37 +0100

    rename docs tasks

--- docs/.kraken.py
+++ docs/.kraken.py
@@ -3,13 +3,13 @@ from docs_helpers import DocsVenvTask, DocsTask
 
 project = Project.current()
 
-docs_venv = project.task("docsVenv", DocsVenvTask)
+docs_venv = project.task("venv", DocsVenvTask)
 docs_venv.directory = project.build_directory / "docs-venev"
 
-docs_build = project.task("docsBuild", DocsTask)
+docs_build = project.task("build", DocsTask)
 docs_build.venv = docs_venv.directory
 docs_build.mode = "build"
 
-docs_serve = project.task("docsServe", DocsTask)
+docs_serve = project.task("serve", DocsTask)
 docs_serve.venv = docs_venv.directory
 docs_serve.mode = "serve"
