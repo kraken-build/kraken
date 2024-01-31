0572f4cea199cf54328b54e0f7b2b51f1c389401
Niklas Rosenstein <niklas.rosenstein@helsing.ai>
2024-01-31 22:03:44 +0100

    add black to docs build for formatting signatures

--- docs/.kraken.py
+++ docs/.kraken.py
@@ -1,7 +1,7 @@
 from kraken.std.docs.tasks.mkdocs import mkdocs
 
 mkdocs(
-    requirements=["mkdocs==1.5.3", "pymdown-extensions", "mkdocstrings[python]", "mkdocs-material"],
+    requirements=["mkdocs==1.5.3", "pymdown-extensions", "mkdocstrings[python]", "mkdocs-material", "black"],
     watch_files=["../kraken-build/src", "../kraken-wrapper/src"],
 )
 
