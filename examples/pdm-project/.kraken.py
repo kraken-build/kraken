a2720112d59d83c3eb524b0988a90584a23ee85e
Niklas Rosenstein <niklas.rosenstein@helsing.ai>
2024-02-02 13:54:08 +0100

    fix example

--- examples/pdm-project/.kraken.py
+++ examples/pdm-project/.kraken.py
@@ -1,18 +1,23 @@
 import os
 
-from kraken.std.python.project import python_library, python_package_index
+# from kraken.std.python.project import python_library, python_package_index
 
-python_package_index(
+# python_package_index(
+#     alias="local",
+#     index_url=os.environ["LOCAL_PACKAGE_INDEX"],
+#     is_package_source=False,
+#     credentials=(os.environ["LOCAL_USER"], os.environ["LOCAL_PASSWORD"]),
+# )
+
+# python_library()
+
+from kraken.std import python
+
+python.python_settings(always_use_managed_env=True).add_package_index(
     alias="local",
     index_url=os.environ["LOCAL_PACKAGE_INDEX"],
-    is_package_source=False,
     credentials=(os.environ["LOCAL_USER"], os.environ["LOCAL_PASSWORD"]),
 )
-
-python_library()
-
-# python.python_settings(always_use_managed_env=True).add_package_index(
-# )
-# python.install()
-# python.mypy()
-# python.publish(package_index="local", distributions=python.build(as_version="0.1.0").output_files)
+python.install()
+python.mypy()
+python.publish(package_index="local", distributions=python.build(as_version="0.1.0").output_files)
