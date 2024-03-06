kraken-build/kraken

commit 22d0c4fa0b8bd7d88e3b25d5c233b332e738cfbf

fix: Revert setting credentials providers by default in `.cargo/config.toml` with the `:cargoSyncConfig` task (#213)

kraken-build/src/kraken/std/cargo/tasks/cargo_sync_config_task.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Sequence
 from pathlib import Path
 from typing import Literal
 
@@ -20,6 +21,11 @@ class CargoSyncConfigTask(RenderFileTask):
     #: If enabled, the configuration file will be replaced rather than updated.
     replace: Property[bool] = Property.default(False)
 
+    #: The global-credential-providers to set in the config. If not set, the config won't be touched. The providers
+    #: must be specified in reverse order of precedence. Read more about credential providers here:
+    #: https://doc.rust-lang.org/cargo/reference/registry-authentication.html
+    global_config_providers: Property[Sequence[str]]
+
     #: The registries to insert into the configuration.
     registries: Property[list[CargoRegistry]] = Property.default_factory(list)
 
@@ -36,15 +42,13 @@ class CargoSyncConfigTask(RenderFileTask):
 
     def get_file_contents(self, file: Path) -> str | bytes:
         content = tomli.loads(file.read_text()) if not self.replace.get() and file.exists() else {}
-        # It seems like credential provider should be provided in reverse order
-        # of preference. We put cargo:token first, to always use the keyring if
-        # it is available.
-        content.setdefault("registry", {})["global-credential-providers"] = [
-            "cargo:token",
-            "cargo:libsecret",
-            "cargo:macos-keychain",
-            "cargo:wincred",
-        ]
+        if self.global_config_providers.is_set():
+            if self.global_config_providers.get() is None:
+                content.setdefault("registry", {}).pop("global-credential-providers", None)
+            else:
+                content.setdefault("registry", {})["global-credential-providers"] = list(
+                    self.global_config_providers.get()
+                )
         content.setdefault("registries", {})["crates-io"] = {"protocol": self.crates_io_protocol.get()}
         for registry in self.registries.get():
             content.setdefault("registries", {})[registry.alias] = {"index": registry.index}

commit be9f3d584c9bd4575b74e5acc0da0622d2267732

fix: Set `cargo:token` by default (fix regression introduced in #213)

kraken-build/src/kraken/std/cargo/tasks/cargo_sync_config_task.py
@@ -24,7 +24,7 @@ class CargoSyncConfigTask(RenderFileTask):
     #: The global-credential-providers to set in the config. If not set, the config won't be touched. The providers
     #: must be specified in reverse order of precedence. Read more about credential providers here:
     #: https://doc.rust-lang.org/cargo/reference/registry-authentication.html
-    global_config_providers: Property[Sequence[str]]
+    global_credential_providers: Property[Sequence[str]] = Property.default(["cargo:token"])
 
     #: The registries to insert into the configuration.
     registries: Property[list[CargoRegistry]] = Property.default_factory(list)
@@ -42,12 +42,12 @@ class CargoSyncConfigTask(RenderFileTask):
 
     def get_file_contents(self, file: Path) -> str | bytes:
         content = tomli.loads(file.read_text()) if not self.replace.get() and file.exists() else {}
-        if self.global_config_providers.is_set():
-            if self.global_config_providers.get() is None:
+        if self.global_credential_providers.is_set():
+            if self.global_credential_providers.get() is None:
                 content.setdefault("registry", {}).pop("global-credential-providers", None)
             else:
                 content.setdefault("registry", {})["global-credential-providers"] = list(
-                    self.global_config_providers.get()
+                    self.global_credential_providers.get()
                 )
         content.setdefault("registries", {})["crates-io"] = {"protocol": self.crates_io_protocol.get()}
         for registry in self.registries.get():