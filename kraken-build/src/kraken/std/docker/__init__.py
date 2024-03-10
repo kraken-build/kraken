7c5462f5cc1bc882595f0a1211bf9f9030ad5a4b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-10 14:25:44 +0100

    fix

--- kraken-build/src/kraken/std/docker/__init__.py
+++ kraken-build/src/kraken/std/docker/__init__.py
@@ -4,12 +4,12 @@ from collections.abc import Sequence
 from pathlib import Path
 from typing import Any
 
-from kraken.common import import_class
-from kraken.core import Project, Task, Property
+from kraken.common import Supplier, import_class
+from kraken.core import Project, Task
 from kraken.std.docker.tasks.base_build_task import BaseBuildTask
 from kraken.std.docker.tasks.manifest_tool_push_task import ManifestToolPushTask
 
-__all__ = ["build_docker_image", "manifest_tool", "sidecar_container"]
+__all__ = ["build_docker_image", "manifest_tool"]
 
 DEFAULT_BUILD_BACKEND = "native"
 BUILD_BACKENDS = {
@@ -24,14 +24,14 @@ def build_docker_image(
     name: str = "buildDocker",
     backend: str = DEFAULT_BUILD_BACKEND,
     project: Project | None = None,
-    dockerfile: str | Path | Property[Path] = "Dockerfile",
+    dockerfile: str | Path | Supplier[Path] = "Dockerfile",
     **kwds: Any,
 ) -> BaseBuildTask:
     """Create a new task in the current project that builds a Docker image and eventually pushes it."""
 
     project = project or Project.current()
     task_class = import_class(BUILD_BACKENDS[backend], BaseBuildTask)  # type: ignore[type-abstract]
-    dockerfile = project.directory / dockerfile
+    dockerfile = Supplier.of(dockerfile).map(project.directory.joinpath)
     return project.do(name, task_class, dockerfile=dockerfile, **kwds)
 
 
