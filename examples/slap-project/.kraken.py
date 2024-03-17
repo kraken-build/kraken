b2a6bf52a400cad66189d270042f4b234a58d49c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-18 00:08:07 +0100

    fix slap-project and make it use python_project()

--- examples/slap-project/.kraken.py
+++ examples/slap-project/.kraken.py
@@ -1,16 +1,11 @@
 import os
 
-from kraken.std import python
+from kraken.std.python.project import python_package_index, python_project
 
-python.python_settings(always_use_managed_env=True).add_package_index(
+python_package_index(
     alias="local",
     index_url=os.environ["LOCAL_PACKAGE_INDEX"],
     credentials=(os.environ["LOCAL_USER"], os.environ["LOCAL_PASSWORD"]),
+    publish=True,
 )
-python.install()
-python.mypy()
-python.flake8()
-python.black()
-python.isort()
-python.pytest()
-python.publish(package_index="local", distributions=python.build(as_version="0.1.0").output_files)
+python_project()
