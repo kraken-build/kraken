55655ab5d394bfb428d190eda2f691b62b442eed
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-22 00:29:12 +0100

    we moved fetch_tarball()

--- kraken-build/src/kraken/std/util/__init__.py
+++ kraken-build/src/kraken/std/util/__init__.py
@@ -3,6 +3,5 @@
 from __future__ import annotations
 
 from .copyright_task import check_and_format_copyright
-from .fetch_tarball_task import fetch_tarball
 
-__all__ = ["check_and_format_copyright", "fetch_tarball"]
+__all__ = ["check_and_format_copyright"]
