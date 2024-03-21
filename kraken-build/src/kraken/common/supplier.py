d9826aa5c7cdbb84a3a3f1e8f0458d7e0be0c4ed
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-21 23:58:01 +0100

    improvement: `Supplier.of()` and related functions now take an `Iterable[Supplier[Any]]` as the `derived_from` parameter instead of a `Sequence` as it is converted to a `tuple` later anyway.

--- kraken-build/src/kraken/common/supplier.py
+++ kraken-build/src/kraken/common/supplier.py
@@ -95,17 +95,17 @@ class Supplier(Generic[T], abc.ABC):
             stack += derived_from
 
     @staticmethod
-    def of(value: "T | Supplier[T]", derived_from: Sequence["Supplier[Any]"] = ()) -> "Supplier[T]":
+    def of(value: "T | Supplier[T]", derived_from: Iterable["Supplier[Any]"] = ()) -> "Supplier[T]":
         if isinstance(value, Supplier):
             return value
         return OfSupplier(value, derived_from)
 
     @staticmethod
-    def of_callable(func: Callable[[], T], derived_from: Sequence["Supplier[Any]"] = ()) -> "Supplier[T]":
+    def of_callable(func: Callable[[], T], derived_from: Iterable["Supplier[Any]"] = ()) -> "Supplier[T]":
         return OfCallableSupplier(func, derived_from)
 
     @staticmethod
-    def void(from_exc: "Exception | None" = None, derived_from: Sequence["Supplier[Any]"] = ()) -> "Supplier[T]":
+    def void(from_exc: "Exception | None" = None, derived_from: Iterable["Supplier[Any]"] = ()) -> "Supplier[T]":
         """Returns a supplier that always raises :class:`Empty`."""
 
         return VoidSupplier(from_exc, derived_from)
@@ -201,9 +201,9 @@ class OnceSupplier(Supplier[T]):
 
 
 class OfCallableSupplier(Supplier[T]):
-    def __init__(self, func: Callable[[], T], derived_from: Sequence[Supplier[Any]]) -> None:
+    def __init__(self, func: Callable[[], T], derived_from: Iterable[Supplier[Any]]) -> None:
         self._func = func
-        self._derived_from = derived_from
+        self._derived_from = tuple(derived_from)
 
     def derived_from(self) -> Iterable[Supplier[Any]]:
         return self._derived_from
@@ -222,7 +222,7 @@ class OfCallableSupplier(Supplier[T]):
 
 
 class OfSupplier(Supplier[T]):
-    def __init__(self, value: T, derived_from: Sequence[Supplier[Any]]) -> None:
+    def __init__(self, value: T, derived_from: Iterable[Supplier[Any]]) -> None:
         self._value = value
         self._derived_from = tuple(derived_from)
 
@@ -243,7 +243,7 @@ class OfSupplier(Supplier[T]):
 
 
 class VoidSupplier(Supplier[T]):
-    def __init__(self, from_exc: "Exception | None", derived_from: Sequence[Supplier[Any]]) -> None:
+    def __init__(self, from_exc: "Exception | None", derived_from: Iterable[Supplier[Any]]) -> None:
         self._from_exc = from_exc
         self._derived_from = tuple(derived_from)
 
