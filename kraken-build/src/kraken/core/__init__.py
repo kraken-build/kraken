16c67f4c758db4b759c786671892b953a58d0876
Niklas Rosenstein <niklas.rosenstein@helsing.ai>
2024-03-21 15:30:48 +0100

    release 0.36.1

--- kraken-build/src/kraken/core/__init__.py
+++ kraken-build/src/kraken/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.36.0"
+__version__ = "0.36.1"
 
 from kraken.core.address import Address
 from kraken.core.system.context import Context, ContextEvent
--- kraken-wrapper/src/kraken/wrapper/__init__.py
+++ kraken-wrapper/src/kraken/wrapper/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "DEFAULT_CONFIG_PATH",
     "AuthModel",
 ]
-__version__ = "0.36.0"
+__version__ = "0.36.1"
