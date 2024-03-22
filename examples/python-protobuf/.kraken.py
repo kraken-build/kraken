6b483fbc62aca0f2d71a2950323dd0c23cba222b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-03-23 00:27:21 +0100

    upodate example

--- examples/python-protobuf/.kraken.py
+++ examples/python-protobuf/.kraken.py
@@ -1,3 +1,5 @@
-from kraken.std.python.project import python_project
+from kraken.build.python.v1alpha1 import python_project
+from kraken.build.protobuf.v1alpha1 import protobuf_project
 
-python_project()
+proto = protobuf_project()
+python_project(codegen = [proto.python])
