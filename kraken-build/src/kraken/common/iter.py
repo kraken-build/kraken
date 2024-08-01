b280979a90029e2db0dc5cafd4eedc9cab46c7e0
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-08-02 01:37:33 +0200

    improve docstring

--- kraken-build/src/kraken/common/iter.py
+++ kraken-build/src/kraken/common/iter.py
@@ -8,6 +8,10 @@ T_co = TypeVar("T_co", covariant=True)
 def bipartition(predicate: Callable[[T_co], bool], it: Iterable[T_co]) -> tuple[Iterable[T_co], Iterable[T_co]]:
     """
     Partition a stream into two separate streams based on a predicate.
+
+    Returns:
+        A tuple of two iterators, where the first iterator contains only the elements for which the *predicate*
+        returned `False`, and the second iterator contains only the elements for which it returned `True`.
     """
 
     t1, t2 = tee(it)
