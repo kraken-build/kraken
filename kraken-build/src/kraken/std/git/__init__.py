44b9041eae8fa29f78577013b5e144c9cee4836b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-08-02 12:45:39 +0200

    deprecation: Deprecate `gitignore(gitignore_io_tokens, gitignore_io_allow_http_request_backfill)`

--- kraken-build/src/kraken/std/git/__init__.py
+++ kraken-build/src/kraken/std/git/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from collections.abc import Sequence
 from pathlib import Path
 from typing import Literal
+import warnings
 
 from kraken.core import Project
 from kraken.std.util.check_file_contents_task import CheckFileContentsTask
@@ -38,6 +39,17 @@ def gitignore(
     """
     Creates a #GitignoreSyncTask and #CheckFileContentsTask for the given project.
     """
+
+    # DEPRECATE: We want to get rid of gitignore.io tokens feature.
+    if gitignore_io_tokens:
+        warnings.warn(
+            "gitignore(gitignore_io_tokens) is deprecated and will be removed in a future version", DeprecationWarning
+        )
+    if gitignore_io_allow_http_request_backfill:
+        warnings.warn(
+            "gitignore(gitignore_io_allow_http_request_backfill) is deprecated and will be removed in a future version",
+            DeprecationWarning,
+        )
 
     project = project or Project.current()
     task = project.task(name, tasks.GitignoreSyncTask, group=group)
