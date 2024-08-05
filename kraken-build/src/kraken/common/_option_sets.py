c82d1aa74581821f4dc38c3180ac75e8a09e53a0
Niklas Rosenstein <rosensteinniklas@gmail.com>
2024-08-05 23:48:13 +0200

    add Loguru intercept handler

--- kraken-build/src/kraken/common/_option_sets.py
+++ kraken-build/src/kraken/common/_option_sets.py
@@ -1,4 +1,5 @@
 import argparse
+import inspect
 import logging
 import sys
 from dataclasses import dataclass
@@ -54,8 +55,25 @@ class LoggingOptions:
         else:
             assert False, verbosity
 
-        # note: this is for components that don't use the `loguru.logger`.
-        logging.basicConfig(level=getattr(logging, level), format="%(asctime)s | %(levelname)s | %(message)s")
+        # Intercept standard logs and send them to Loguru.
+        class InterceptHandler(logging.Handler):
+            def emit(self, record: logging.LogRecord) -> None:
+                # Get corresponding Loguru level if it exists.
+                level: str | int
+                try:
+                    level = logger.level(record.levelname).name
+                except ValueError:
+                    level = record.levelno
+
+                # Find caller from where originated the logged message.
+                frame, depth = inspect.currentframe(), 0
+                while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+                    frame = frame.f_back
+                    depth += 1
+
+                logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+        logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
 
         logger.remove()
         logger.add(sys.stderr, level=level)
