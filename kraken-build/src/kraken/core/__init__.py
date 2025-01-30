1409f77d0866ea18d2d12bfcd8da85a2477e7197
thomas.pellissier-tanon <thomas.pellissier-tanon@helsing.ai>
2025-01-30 17:45:41 +0100

    release 0.41.1

--- kraken-build/src/kraken/core/__init__.py
+++ kraken-build/src/kraken/core/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.41.0"
+__version__ = "0.41.1"
 
 from kraken.core.address import Address
 from kraken.core.system.context import Context, ContextEvent
--- kraken-wrapper/src/kraken/wrapper/__init__.py
+++ kraken-wrapper/src/kraken/wrapper/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "DEFAULT_CONFIG_PATH",
     "AuthModel",
 ]
-__version__ = "0.41.0"
+__version__ = "0.41.1"
