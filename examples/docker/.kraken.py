49c41f3317f2cf7b62bdb9828ebc76a608df710c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-07-09 20:24:45 +0200

    fix docker example

--- examples/docker/.kraken.py
+++ examples/docker/.kraken.py
@@ -1,13 +1,13 @@
 from kraken.core import Project
 
 from kraken.std.docker import build_docker_image
-from kraken.std.generic.render_file import RenderFileTask
+from kraken.std.util.render_file_task import RenderFileTask
 
 project = Project.current()
 
 dockerfile = project.task("dockerfile", RenderFileTask)
 dockerfile.content.set("FROM ubuntu:focal\nRUN echo Hello world\n")
-dockerfile.file.set(project.build_directory / "Dockerfile"),
+dockerfile.file.set(project.build_directory / "Dockerfile")
 
 build = build_docker_image(
     name="buildDocker",
