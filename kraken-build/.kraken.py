78e65bb4ee86e0ac7a269f23a92ff950923f7551
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-07-09 22:04:51 +0200

    update pytest config

--- kraken-build/.kraken.py
+++ kraken-build/.kraken.py
@@ -1,3 +1,3 @@
 from kraken.std import python
 
-python.pytest(ignore_dirs=["src/tests/integration"], include_dirs=["src/kraken/build"])
+python.pytest(ignore_dirs=["tests/kraken_std/data"], include_dirs=["kraken/build"])
