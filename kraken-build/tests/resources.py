0671d579a78711a68a07b538a8813f57abc7593a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-07-10 08:48:17 +0200

    fix docstring

--- kraken-build/tests/resources.py
+++ kraken-build/tests/resources.py
@@ -6,6 +6,6 @@ from pathlib import Path
 
 
 def data_path(name: str) -> Path:
-    """Returns the path to the example directory."""
+    """Returns the path to a file or directory in the tests data directory."""
 
     return (Path(__file__).parent / "data" / name).resolve()
