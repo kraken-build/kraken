5294a41af73cbe58a27332d938828b42c910e53c
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-07-10 10:20:13 +0200

    simplify kraken's own build scripts

--- kraken-wrapper/.kraken.py
+++ kraken-wrapper/.kraken.py
@@ -1,5 +1,8 @@
 from kraken.std import python
 
+python.install()
+python.mypy(version_spec="==1.16.1")
+python.ruff(additional_args=["--exclude", "tests/iss-263/example_project"])
 python.pytest(
     ignore_dirs=[
         "tests/iss-263/dependency",
