4744c2903086ca81c519a814822d753c864b401b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-07-10 13:43:09 +0200

    Add `scripts/bump.py` to reduce dependency on Slap (#364)
    * Add `scripts/bump.py` to reduce dependency on Slap
    
    * slight change to .kraken.py
    
    * fix eval example

--- .kraken.py
+++ .kraken.py
@@ -3,9 +3,9 @@ from kraken.common import buildscript
 buildscript(requirements=["kraken-build>=0.44.0"])
 
 from kraken.build import project  # noqa: E402
-from kraken.std.git import gitignore  # noqa: E402
+from kraken.std import git  # noqa: E402
 
-gitignore()
+git.gitignore()
 project.subproject("docs")
 project.subproject("kraken-build")
 project.subproject("kraken-wrapper")
