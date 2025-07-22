95063feff34bd414d8d18a5bfc41f7d28130533d
Niklas Rosenstein <rosensteinniklas@gmail.com>
2025-07-22 14:02:47 +0200

    split over lines

--- kraken-wrapper/src/kraken/wrapper/uv_venv.py
+++ kraken-wrapper/src/kraken/wrapper/uv_venv.py
@@ -247,7 +247,9 @@ class UvProjectShim:
         command = ["uv", "sync", "--project", self._tempdir.name, "--python", fspath(venv.python_bin)]
         logger.debug("Installing into build environment with uv: %s", sanitize_http_basic_auth(" ".join(command)))
         subprocess.check_call(
-            command, cwd=self._tempdir.name, env=os.environ | {"UV_PROJECT_ENVIRONMENT": fspath(venv.path)}
+            command,
+            cwd=self._tempdir.name,
+            env=os.environ | {"UV_PROJECT_ENVIRONMENT": fspath(venv.path)},
         )
 
     def lock(self, venv: UvVirtualEnv) -> None:
@@ -256,7 +258,9 @@ class UvProjectShim:
         command = ["uv", "lock", "--project", self._tempdir.name, "--python", fspath(venv.python_bin)]
         logger.debug("Locking build environment with uv: %s", sanitize_http_basic_auth(" ".join(command)))
         subprocess.check_call(
-            command, cwd=self._tempdir.name, env=os.environ | {"UV_PROJECT_ENVIRONMENT": fspath(venv.path)}
+            command,
+            cwd=self._tempdir.name,
+            env=os.environ | {"UV_PROJECT_ENVIRONMENT": fspath(venv.path)},
         )
 
     def pyproject_toml(self) -> Path:
