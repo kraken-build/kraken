c194527c6affea2e32f15b43e9a08633291dd7cb
Thomas Pellissier-Tanon <thomas.pellissier-tanon@helsing.ai>
2025-10-17 15:33:00 +0200

    Comments

--- kraken-build/.kraken.py
+++ kraken-build/.kraken.py
@@ -1,7 +1,7 @@
 from kraken.std import python
 
 python.install()
-python.mypy(version_spec="==1.18.2", python_version="3.12")
+python.mypy(version_spec="==1.18.2", python_version="3.12")  # mitmproxy latest version requires 3.12
 python.ruff(additional_args=["--exclude", "tests/data"])
 python.pytest(
     tests_dir=["src", "tests/kraken_core"],
