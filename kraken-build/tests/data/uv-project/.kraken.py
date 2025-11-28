7217f0b90c42671cfd685e9778846e4b00009d4d
Thomas Pellissier-Tanon <thomas.pellissier-tanon@helsing.ai>
2025-11-28 14:55:37 +0100

    Adds basic support of Ty
    Ty seems to move forward nicely

--- kraken-build/tests/data/uv-project/.kraken.py
+++ kraken-build/tests/data/uv-project/.kraken.py
@@ -8,6 +8,6 @@ python.python_settings(always_use_managed_env=True).add_package_index(
     credentials=(os.environ["LOCAL_USER"], os.environ["LOCAL_PASSWORD"]),
 )
 python.install()
-python.mypy(version_spec="==1.10.0")
+python.ty(version_spec="==0.0.1a28")
 python.ruff(version_spec="==0.9.4")
 python.publish(package_index="local", distributions=python.build(as_version="0.1.0").output_files)
