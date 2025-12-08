a34394ba023ed9cc2faf440d5a1e5c41e1180d40
John Pocock <John-P@users.noreply.github.com>
2025-12-08 12:06:04 +0000

    Fix url join and add debug logs

--- kraken-build/src/kraken/std/python/tasks/publish_task.py
+++ kraken-build/src/kraken/std/python/tasks/publish_task.py
@@ -121,7 +121,9 @@ def _get_existing_files_from_index(
     # PEP 503: Names should be normalized.
     # See https://packaging.python.org/en/latest/specifications/name-normalization/#name-normalization
     normalized_name = canonicalize_name(project_name)
-    url = urllib.parse.urljoin(index_url, f"/{normalized_name}/")
+    logger.debug(f"Index URL: {index_url}")
+    url = urllib.parse.urljoin(f"{index_url}/", normalized_name)
+    logger.debug(f"Normalised package index URL: {url}")
     # For valid Content-Types,
     # see https://packaging.python.org/en/latest/specifications/simple-repository-api/#content-types
     headers = {
